6559dc07febdaffd1933337901f754613fa7003b
wooseungnoh <50711003+wooseungnoh@users.noreply.github.com>
2019-11-18 10:35:44 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,17 +1,16 @@
-const clock = document.querySelector('h2');
+var clock = document.querySelector('h2');
 
-function day(){
-	const newDay = new Date(),
-		hours = newDay.getHours(),
-		minutes = newDay.getMinutes(),
-		seconds = newDay.getSeconds();
-
-		clock.innerText = `${hours<10?`0${hours}`:`${hours}`}:${minutes<10?`0${minutes}`:`${minutes}`}:${seconds<10?`0${seconds}`:`${seconds}`}`
+function day() {
+  var newDay = new Date(),
+      hours = newDay.getHours(),
+      minutes = newDay.getMinutes(),
+      seconds = newDay.getSeconds();
+  clock.innerText = "".concat(hours < 10 ? "0".concat(hours) : "".concat(hours), ":").concat(minutes < 10 ? "0".concat(minutes) : "".concat(minutes), ":").concat(seconds < 10 ? "0".concat(seconds) : "".concat(seconds));
 }
 
-function init(){
-day();
-setInterval(day, 1000)
+function init() {
+  day();
+  setInterval(day, 1000);
 }
 
 init();
